e85aae916368357f89f3f25e5b0b8bdb744057f7
O1O-jpg <520wmwm1314@gmail.com>
2026-01-22 21:44:15 +0800

    2026-1-22

---
+++ Python/2026/Jan/[数据类型转换]/数据类型转换.py
@@ -0,0 +1,17 @@
+# 数字转字符串
+a = 123
+print(type(a),a)
+print(type(str(a)),str(a))
+
+b = 12.3
+print(type(b),b)
+print(type(str(b)),str(b))
+
+# int与float互转
+
+print(type(a),a)
+print(type(float(a)),float(a))
+
+print(type(b),b)
+print(type(int(b)),int(b))
+
